41b81d5ee019ee568efea6c986a099cf845824a4
Vijay Rana <vinayrana596@gmail.com>
2023-03-31 01:20:36 +0530

    added comma in urls

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from firstapp import views 
 urlpatterns = [
-    # path("", views.index, name='index')
-    # path("about", views.about, name='about')
-    path("contact", views.contact, name='contact')
+    path("", views.index, name='index'),
+    path("about", views.about, name='about'),
+    path("contact", views.contact, name='contact'),
     # path("services", views.services, name='service')
 ]
